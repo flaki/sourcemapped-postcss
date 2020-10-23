0d074bfedb47f67fa3140f7af5355b5d929a7ee7
Flaki <git@flaki.hu>
2020-10-23 15:54:15 +0300

    Remove rogue console.log

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const postCSSGenerate = require("./src/app");
 
 module.exports = function(eleventyConfig) {
-console.log(eleventyConfig)
   /* Trigger CSS asset pipeline on rebuild */
   eleventyConfig.on('beforeWatch', () => {
     return postCSSGenerate();
